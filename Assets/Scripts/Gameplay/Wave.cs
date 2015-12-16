197328e2e83f112019fd47d4edb531f3199947a4
Morco <morco.mail@gmail.com>
2015-12-16 23:24:39 +0200

    preview to waves

--- Assets/Scripts/Gameplay/Wave.cs
+++ Assets/Scripts/Gameplay/Wave.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class Wave : MonoBehaviour
 {
 	public LineRenderer graphics;
+	public float		moveSpeed;
+	public float		timeToLive;
+	public float		timeToReady;
+	public float		previewAmount;
+
 
 	[HideInInspector]	public int		colorIndex;
-	[HideInInspector]	public float	moveSpeed;
-
 
 	Vector3 _moveDirection;
+	bool    _isReady = false;
 
 
 	void Start ()
@@ -19,11 +23,28 @@ public class Wave : MonoBehaviour
 		graphics.SetColors( colorStart, colorEnd );
 
 		 _moveDirection = transform.rotation * Vector3.left;
+
+		StartCoroutine( _GetReady() );
+
+		Destroy( gameObject, timeToLive );
 	}
 	
 
 	void Update ()
 	{
-		transform.Translate( _moveDirection * moveSpeed * Time.deltaTime, Space.World );
+		if ( _isReady )
+		{
+			transform.Translate( _moveDirection * moveSpeed * Time.deltaTime, Space.World );
+		}
+	}
+
+
+	IEnumerator _GetReady()
+	{
+		transform.Translate( _moveDirection * previewAmount, Space.World );
+
+		yield return new WaitForSeconds( timeToReady );
+
+		_isReady = true;
 	}
 }
--- Assets/Scripts/Gameplay/WaveSpawner.cs
+++ Assets/Scripts/Gameplay/WaveSpawner.cs
@@ -5,8 +5,8 @@ public class WaveSpawner : MonoBehaviour
 {
 	public Wave		wavePrefab;
 	public float	spawnDelay;
-	public float    moveSpeed;
-	public float    waveLife;
+	public float	cornerOffset;
+	
 
 
 	Vector3[]		_startPoints	= new Vector3[8];
@@ -27,10 +27,10 @@ public class WaveSpawner : MonoBehaviour
 		_startPoints[1] = - mostRight;
 		_startPoints[2] = mostTop;
 		_startPoints[3] = - mostTop;
-		_startPoints[4] = mostTop + mostRight;
-		_startPoints[5] = mostTop - mostRight;
-		_startPoints[6] = - mostTop + mostRight;
-		_startPoints[7] = - mostTop - mostRight;
+		_startPoints[4] = mostTop + mostRight + new Vector3( - cornerOffset, - cornerOffset, 0 );
+		_startPoints[5] = mostTop - mostRight + new Vector3( cornerOffset, - cornerOffset, 0 );
+		_startPoints[6] = - mostTop + mostRight + new Vector3( - cornerOffset, cornerOffset, 0 );
+		_startPoints[7] = - mostTop - mostRight + new Vector3( cornerOffset, cornerOffset, 0 );
 
 		_rotations[0] = Quaternion.Euler( 0, 0, 0 );
 		_rotations[1] = Quaternion.Euler( 0, 0, 180 );
@@ -65,12 +65,9 @@ public class WaveSpawner : MonoBehaviour
 		int chosenColor = Random.Range( 0, GameplayManager.singleton.currLevel.colors.Count );
 
 		Wave wave = Instantiate( wavePrefab );
-		wave.moveSpeed			= moveSpeed;
 		wave.colorIndex			= GameplayManager.singleton.currLevel.colors[chosenColor].colorIndex;
 		wave.transform.position = _startPoints[chosenStart];
 		wave.transform.rotation = _rotations[chosenStart];
 		wave.transform.SetParent( transform );
-
-		Destroy( wave.gameObject, waveLife );
 	}
 }
